649adff0b93b77fa3dc080e56a1f8cdfb5abf4ba
Aniruddha Wankhade <65590325+aniruddhapw@users.noreply.github.com>
2023-01-24 23:25:51 +0530

    form fixed
    form gets blank after submit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
   return (
     <div name="contact" className="w-full h-screen bg-white text-white">
       <div className="flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full">
@@ -22,23 +34,33 @@ const Contact = () => {
             <input
               type="text"
               name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
               placeholder="Enter your name"
               className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
             />
             <input
               type="email"
               name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter your email"
               className="my-4 p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
             />
             <textarea
               name="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
               placeholder="Enter your message"
               rows="10"
               className="p-2 bg-transparent border-2 rounded-md text-black focus:outline-none"
             ></textarea>
 
-            <button className="text-white bg-gradient-to-r  from-stone-800 to-slate-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300">
+            <button
+              type="submit"
+              onSubmit={handleSubmit}
+              className="text-white bg-gradient-to-r  from-stone-800 to-slate-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+            >
               Let's talk
             </button>
           </form>
@@ -49,3 +71,4 @@ const Contact = () => {
 };
 
 export default Contact;
+
