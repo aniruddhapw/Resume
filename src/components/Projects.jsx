46b05c0f500837bf9bb1133b99df0137cbaa3d62
Aniruddha Wankhade <65590325+aniruddhapw@users.noreply.github.com>
2023-01-25 12:27:39 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -8,21 +8,25 @@ const Projects = () => {
     {
       id: 1,
       src: amazon,
+      link1: "https://clone-fa3a8.firebaseapp.com/",
+      link2: "https://www.github.com/aniruddhapw/",
     },
     {
       id: 2,
       src: foa,
+      link1: "https://www.friendsofanimals.org.in/",
+      link2: "https://www.github.com/aniruddhapw/",
     },
     {
       id: 3,
       src: netflix,
+      link1: "https://netflix-clone-85529.firebaseapp.com/",
+      link2: "https://www.github.com/aniruddhapw/",
     },
   ];
-  const handleClick = () => {
-    window.location.href = "https://clone-fa3a8.firebaseapp.com/";
-  };
-  const handleClick1 = () => {
-    window.location.href = "https://github.com/aniruddhapw";
+
+  const handleClick = (link) => {
+    window.open(link);
   };
 
   return (
@@ -36,23 +40,22 @@ const Projects = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {Projects.map(({ id, src }) => (
+          {Projects.map(({ id, src, link1, link2 }) => (
             <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                href="https://clone-fa3a8.firebaseapp.com/"
                 className="rounded-md duration-200 hover:scale-105"
               />
               <div className="flex items-center justify-center">
                 <button
-                  onClick={handleClick}
+                  onClick={() => handleClick(link1)}
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:font-bold"
                 >
                   Demo
                 </button>
                 <button
-                  onClick={handleClick1}
+                  onClick={() => handleClick(link2)}
                   className="w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105 hover:font-bold"
                 >
                   Code
@@ -67,3 +70,4 @@ const Projects = () => {
 };
 
 export default Projects;
+
